8eaab8d4713de112ed61e1f6b515028d638db1f0
10001afifnurilI <afif.nuril17@gmail.com>
2020-07-13 09:50:35 +0700

    menambah index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <table>
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+
+    <tr>
+      <td>1</td>
+      <td><img src="img/afif.jpeg"></td>
+      <td>15650104</td>
+      <td>Afif Nuril Ihsan</td>
+      <td>afif.nuril17@gmail.com</td>
+      <td>Teknik Informatika</td>
+      <td>
+        <a href="">Ubah</a> <a href="">Hapus</a>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
